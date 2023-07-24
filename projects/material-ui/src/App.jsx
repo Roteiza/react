4532158b4d4ee706afcd33d77fecb84ac9aefd9e
Rodrigo Oteiza <roteiza@outlook.com>
2023-07-23 23:18:26 -0400

    Project 03 Mouse Follower completed

---
+++ projects/material-ui/src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@mui/material";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>App</h1>
+        <Button variant="contained">Hello World</Button>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
